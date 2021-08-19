31e62a12f81a76845307d6c93b8555a85b9c30d2
Lucas Seijo <lucaseijo7@gmail.com>
2021-08-19 00:42:57 -0300

    validacion de formulario agregada

--- src/Comps/Formulario.js
+++ src/Comps/Formulario.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Formulario = ({cantidad, guardarCantidad}) => {
+const Formulario = ({cantidad, guardarCantidad, plazo, guardarPlazo}) => {
+
+    // Definir State
+    const [error, guardarError] = useState(false);
+
+    // Cuando el usuario hace submit
+    const calcularPrestamo = e => {
+        e.preventDefault();
+
+        if (plazo === '' || cantidad === 0){
+            guardarError(true);
+            console.log('Error en validación');
+            return;
+        }
+
+    }
 
     return ( 
-        <form>
+        <form onSubmit={calcularPrestamo}>
           <div className="row">
               <div>
                   <label>Cantidad Prestamo</label>
@@ -18,7 +33,9 @@ const Formulario = ({cantidad, guardarCantidad}) => {
                   <label>Plazo para Pagar</label>
                   <select 
                       className="u-full-width"
+                      onChange={ e => guardarPlazo( parseInt(e.target.value) )}
                   >
+                      {plazo}
                       <option value="">Seleccionar</option>
                       <option value="3">3 meses</option>
                       <option value="6">6 meses</option>
